b6418a612785068dd826f672aed92295c53317a5
ujjwal-shekhar <95298765+ujjwal-shekhar@users.noreply.github.com>
2023-11-18 17:55:22 +0530

    Testing files, ran on Ada, waiting for output
    - [] BlogCatalog dataset
    - [X] Karate Dataset
    - [] Other datasets extension

---
+++ blogcatalog/blogcatalog.py
@@ -0,0 +1,241 @@
+import scipy.io as sio
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.autograd import Variable
+import matplotlib.pyplot as plt
+import networkx as nx
+from sklearn.manifold import TSNE
+from gensim.models import Word2Vec
+import multiprocessing
+import logging  # Setting up the loggings to monitor gensim
+from sklearn.preprocessing import MultiLabelBinarizer
+
+# Train a one-vs-rest logistic regression classifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.multiclass import OneVsRestClassifier
+from sklearn.metrics import f1_score, accuracy_score
+from sklearn.model_selection import train_test_split
+
+import sys
+
+cores = multiprocessing.cpu_count() # Count the number of cores in a computer
+
+class DeepWalk():
+        def __init__(
+                        self, 
+                        graph : "list[list[int]]", 
+                        window_size : int, 
+                        embedding_size : int, 
+                        walks_per_vertex : int, 
+                        walk_length : int
+                ) -> None:
+                """
+                Initialize the DeepWalk model. This directly from the paper https://arxiv.org/pdf/1403.6652.pdf.
+
+                Parameters
+                ----------
+                graph : list[list[int]]
+                        The adjacency list to be embedded. This is a list of lists, where each list is a vertex and its neighbors.
+                window_size : int
+                        The window size for the skipgram model.
+                embedding_size : int
+                        The size of the embedding. The final output matrix will be of size |V| x embedding_size.
+                walks_per_vertex : int
+                        The number of walks to perform per vertex.
+                walk_length : int
+                        The length of each walk.
+
+                Methods
+                -------
+                generate_n_walks()
+                        Generate n walks from the graph.
+                train()
+                        Train the model.
+                update()
+                        Feed model new walks.
+                get_embeddings()
+                        Return the embeddings.
+                """
+
+                # DeepWalk parameters
+                self.g = graph
+                self.w = window_size
+                self.d = embedding_size
+                self.gamma = walks_per_vertex
+                self.epochs = self.gamma
+                self.t = walk_length
+                self.n = len(graph)
+
+        def unbiased_random_walk(
+                self,
+                adj_mat : 'list[list[int]]', 
+                walk_len : 'int', 
+                start_node : 'int'
+        ) -> np.array:
+                """
+                Returns a random walk of length walk_len from start_node in the graph.
+
+                Parameters
+                ----------
+                adj_mat : list[list[int]]
+                        Adjacency matrix of the graph.
+                walk_len : int
+                        Length of the random walk.
+                start_node : int
+                        Starting node of the random walk.
+
+                Returns
+                -------
+                np.array
+                        List of nodes in the random walk.        
+                """
+                # Array to store the walk
+                walk = [start_node]
+
+                # Make an adjacency matrix to easily 
+                # sample the neighbors of a node.
+                adj_list = []
+                for i in range(len(adj_mat)):
+                        # This line was autocompleted by GH Copilot
+                        adj_list.append(np.where(adj_mat[i] > 0)[0])
+
+                # Keep walking for walk_len epochs
+                for epochs in range(walk_len):
+                        # Randomly pick a node from the adj_list[walk[-1]] 
+                        # and append it to the walk
+                        walk.append(np.random.choice(adj_list[walk[-1]]))
+
+                return np.array(walk)
+
+        def generate_n_walks(self, num_iters : int) -> 'list[list[str]]':
+                """
+                Generate a list of num_iters random walks. These will be used to train the model
+
+                Parameters
+                ----------
+                num_iters : int
+                        Number of walks to generate.
+
+                Returns
+                -------
+                np.ndarray
+                        List of random walks.
+                """
+
+                # List to store the walks
+                walks = []
+
+                # For each vertex in the graph
+                for vertex in range(self.n):
+                        # Generate gamma walks of length t
+                        for _ in range(self.gamma):
+                                walks.append(self.unbiased_random_walk(self.g, self.t, vertex))
+
+                walks = [[str(node) for node in walk] for walk in walks]
+
+                return walks
+
+        def train(self, epochs : int, lr : float) -> None:
+                """
+                Train the model.
+
+                Parameters
+                ----------
+                epochs : int
+                        Number of epochs to train the model for.
+                lr : float
+                        Learning rate for the optimizer.                
+                """
+
+                # Generate many walks
+                walks = self.generate_n_walks(self.gamma)
+
+                # Initialize the model
+                self.model = Word2Vec(
+                        walks,
+                        hs=1,
+                        sg=1,
+                        negative=0,
+                        alpha=0.05,
+                        epochs=self.epochs, 
+                        vector_size=self.d,        # embedding dimension
+                        window=self.w,             # context window size
+                        min_count=0,
+                        workers=cores-2
+                )
+
+        def get_embeddings(self) -> np.ndarray:
+                """
+                Return the embeddings.
+
+                Returns
+                -------
+                np.ndarray
+                        Embeddings.
+                """
+                return [self.model.wv[str(n)] for n in range(self.n)]
+        
+        def plot_embeddings(self, num_dimensions : int, gt_labels : 'list[str]') -> None:
+                """
+                Plot the embeddings.
+
+                Parameters
+                ----------
+                num_dimensions : int
+                        Number of dimensions to plot.
+                gt_labels : list[str]
+                        List of ground truth labels.
+                """
+                embeddings = np.array(self.get_embeddings())
+
+                # dimensionality reduction to 2 dimensions using t-SNE for visualization
+                embeddings = TSNE(n_components=2).fit_transform(embeddings)
+
+                # Convert gt into numbers
+                gt_labels = np.array(gt_labels)
+
+                # Iterate over all labels and assign them an integer
+                for i, label in enumerate(np.unique(gt_labels)):
+                        gt_labels[gt_labels == label] = i
+                gt_labels = gt_labels.astype(int)
+
+                # plot the embeddings
+                plt.figure(figsize=(6,6))
+                plt.scatter(embeddings[:,0], embeddings[:,1], c=gt_labels)
+
+                # Plot node numbers
+                for i in range(len(embeddings)):
+                        plt.annotate(i , (embeddings[i,0], embeddings[i,1]))
+                        
+                plt.show()
+
+# load a .mat file into a numpy array
+def load_mat(filename):
+    data = sio.loadmat(filename)
+    return data
+
+
+def main():
+    print("Number of cores present: ", cores, file=sys.stdout)
+    
+    logging.basicConfig(format="%(levelname)s - %(asctime)s: %(message)s", level=logging.INFO,filename='blogcatalog.log',filemode='w')
+
+    data_np = load_mat('/home2/anika.roy/SMAI-project/datasets/blogcatalog.mat')
+    data_np = data_np['network']
+
+    # Convert the sparse matrix to a dense adjacency matrix
+    adjmat = data_np.toarray()
+
+    # adjmat, window_size, embedding_size, walks_per_vertex, walk_length
+    dw = DeepWalk(adjmat, 10, 128, 80, 40)
+
+    dw.train(20, 0.05)
+
+    # output embeddings to an output file
+    dw.model.wv.save_word2vec_format('blogcatalog_embeddings.txt')
+
+
+if __name__ == "__main__":
+    main()
