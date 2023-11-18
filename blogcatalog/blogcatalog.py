4711c70dc9fe2dd9de79fa1a82b8452597c9f0f5
Anika Roy <anika1234roy@gmail.com>
2023-11-19 00:11:09 +0530

    updated deepwalk to include aliasing to run faster

--- blogcatalog/blogcatalog.py
+++ blogcatalog/blogcatalog.py
@@ -29,7 +29,9 @@ class DeepWalk():
                         window_size : int, 
                         embedding_size : int, 
                         walks_per_vertex : int, 
-                        walk_length : int
+                        walk_length : int,
+                        p : float,
+                        q : float
                 ) -> None:
                 """
                 Initialize the DeepWalk model. This directly from the paper https://arxiv.org/pdf/1403.6652.pdf.
@@ -46,6 +48,10 @@ class DeepWalk():
                         The number of walks to perform per vertex.
                 walk_length : int
                         The length of each walk.
+                p : float
+                        The return parameter.
+                q : float
+                        The in-out parameter.
 
                 Methods
                 -------
@@ -67,15 +73,59 @@ class DeepWalk():
                 self.epochs = self.gamma
                 self.t = walk_length
                 self.n = len(graph)
+                self.p = p
+                self.q = q
+                
+                # Cutoffs for sampling
+                self.p0 = 1 / self.p / max(1, self.p, self.q)
+                self.p1 = 1 /  max(1, self.p, self.q)
+                self.p2 = 1 / self.q / max(1, self.p, self.q)
+                
+                # Make the adjacency list using the graph
+                self.adj_list = []
+                for i in range(len(self.g)):
+                    neighbors = np.where(self.g[i] > 0)[0]  # Get neighbor indices
+                    weights = self.g[i, neighbors]  # Get corresponding edge weights
+    
+                    # Sort neighbors and weights by weight
+                    sorted_neighbors_weights = sorted(zip(neighbors, weights), key=lambda x: x[0])
+                    sorted_neighbors, sorted_weights = zip(*sorted_neighbors_weights)
 
-        def unbiased_random_walk(
-                self,
-                adj_mat : 'list[list[int]]', 
-                walk_len : 'int', 
-                start_node : 'int'
-        ) -> np.array:
+                    self.adj_list.append(list(zip(sorted_neighbors, sorted_weights / np.sum(sorted_weights))))
+        
+        def get_random_neighbour(self, vertex : 'int') -> 'int':
                 """
-                Returns a random walk of length walk_len from start_node in the graph.
+                Fetches a random neighbour of a given vertex
+                by sampling on the basis of the edge weights
+                
+                Parameters
+                ----------
+                vertex : int
+                        The vertex whose neighbour we will sample
+                        
+                Returns
+                -------
+                int
+                        The neighbour that was sampled
+                """
+                # Sample a neighbour with probability proportional
+                # to the edge weight from vertex --> neighbour
+                neighbours, weights = zip(*self.adj_list[vertex])
+                
+                # Sample a vertex with probability proportional 
+                # to the weight of the edge joining it.
+                return np.random.choice(neighbours, p=weights)
+                    
+        def second_order_biased_random_walk(
+                        self,
+                        adj_mat : 'list[list[int]]', 
+                        walk_len : 'int', 
+                        start_node : 'int', 
+                        return_parameter :'float', 
+                        in_out_parameter : 'float'
+                        ) -> np.array:
+                """
+                Return a walk based on a 2nd order Markov Chain like transition.
 
                 Parameters
                 ----------
@@ -85,29 +135,50 @@ class DeepWalk():
                         Length of the random walk.
                 start_node : int
                         Starting node of the random walk.
-
+                return_parameter : float
+                        The value of the "p" parameter
+                in_out_parameter : float
+                        The value of the "q" parameter
+                
                 Returns
                 -------
                 np.array
-                        List of nodes in the random walk.        
+                        List of nodes in the random walk.  
+                
                 """
                 # Array to store the walk
-                walk = [start_node]
+                walk = [
+                        start_node,
+                        self.get_random_neighbour(start_node) # The prev_node is never Null
+                ]
 
-                # Make an adjacency matrix to easily 
-                # sample the neighbors of a node.
-                adj_list = []
-                for i in range(len(adj_mat)):
-                        # This line was autocompleted by GH Copilot
-                        adj_list.append(np.where(adj_mat[i] > 0)[0])
-
-                # Keep walking for walk_len epochs
-                for epochs in range(walk_len):
-                        # Randomly pick a node from the adj_list[walk[-1]] 
-                        # and append it to the walk
-                        walk.append(np.random.choice(adj_list[walk[-1]]))
-
-                return np.array(walk)
+                # Generate the rest of the walk
+                for i in range(2, walk_len):
+                    # Variable to check whether we added to walk
+                    found = False
+                    
+                    # Kep running until sampled in the red region
+                    while not found:
+                        new_node = self.get_random_neighbour(walk[-1])
+                        r = np.random.rand()
+                        
+                        # Check if we will go back to the same node
+                        if new_node == walk[-2]:
+                            if r < self.p0:
+                                found = True
+                        
+                        # Check if we are going to move by a distance of 1
+                        elif self.g[walk[-2]][new_node]:
+                            if r < self.p1:
+                                found = True
+                                    
+                        else: # So we are moving by a distance of 2
+                            if r < self.p2:
+                                found = True
+                                
+                    walk.append(new_node)
+            
+                return walk
 
         def generate_n_walks(self, num_iters : int) -> 'list[list[str]]':
                 """
@@ -126,12 +197,17 @@ class DeepWalk():
 
                 # List to store the walks
                 walks = []
-
+                
+                counter = 0
                 # For each vertex in the graph
                 for vertex in range(self.n):
-                        # Generate gamma walks of length t
+                        # Generate gamma walks of length t starting from this "vertex"
                         for _ in range(self.gamma):
-                                walks.append(self.unbiased_random_walk(self.g, self.t, vertex))
+                                walks.append(self.second_order_biased_random_walk(self.g, self.t, vertex, self.p, self.q))
+
+                        counter += 1
+                        if(counter % 100 == 0):
+                                print(f"walks generated for {counter} nodes", file=sys.stdout)
 
                 walks = [[str(node) for node in walk] for walk in walks]
 
@@ -147,8 +223,7 @@ class DeepWalk():
                         Number of epochs to train the model for.
                 lr : float
                         Learning rate for the optimizer.                
-                """
-
+                """          
                 # Generate many walks
                 walks = self.generate_n_walks(self.gamma)
 
@@ -228,8 +303,8 @@ def main():
     # Convert the sparse matrix to a dense adjacency matrix
     adjmat = data_np.toarray()
 
-    # adjmat, window_size, embedding_size, walks_per_vertex, walk_length
-    dw = DeepWalk(adjmat, 10, 128, 80, 40)
+    # adjmat, window_size, embedding_size, walks_per_vertex, walk_length, p and q
+    dw = DeepWalk(adjmat, 10, 128, 80, 40,1,1)
 
     dw.train(20, 0.05)
 
