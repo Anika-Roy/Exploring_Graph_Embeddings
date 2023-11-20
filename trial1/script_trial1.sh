ceed7c66bb7758ae8f004e610ce9bdd2e27304ff
Anika Roy <anika1234roy@gmail.com>
2023-11-21 00:12:40 +0530

    some progress made with classification

---
+++ trial1/script_trial1.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --output=output.out
+#SBATCH --error=error.err
+#SBATCH --time=4-00:00:00
+#SBATCH --mail-user=chipakupoy@gmail.com
+#SBATCH --mail-type=ALL
+
+echo "Starting the job at $(date)"
+
+source /home2/anika.roy/miniconda3/bin/activate ML
+python3 -u trial1.py
+
+echo "Job finished at $(date)"
